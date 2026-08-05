339b621eb381af3f26488de48c42e02429eb8dfa
Arief Setyo Nugroho <asnproject02@gmail.com>
2026-08-05 17:06:35 +0700

    update

--- screens/sidebar/dashboard.py
+++ screens/sidebar/dashboard.py
@@ -292,6 +292,7 @@ class Dashboard(QWidget):
                 control_type=control.get("type", "switch"),
                 state=control.get("default", False),
                 button_text=control.get("text", "Execute"),
+                value=control.get("value", None)
             )
 
             card.toggled.connect(self.control_changed)
@@ -315,11 +316,12 @@ class Dashboard(QWidget):
             }
         )
 
-    def control_pressed(self, key):
+    def control_pressed(self, key, value):
 
         self.serial.send({
             "type": "button",
             "key": key,
+            "value": value,
         })
 
     def process_packet(self, packet):
